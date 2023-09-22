73dd951a9318f7adcbeefda897556139acb66f55
dotofficium <dotofficium@gmail.com>
2023-09-22 10:55:33 +0530

    - Added template

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("mypage/", views.show, name="my-page"),
     path("addition/<int:a>/<int:b>/", views.add, name="addition"),
     path("fact/<int:num>/", views.fact, name="fact"),
+    path("data/", views.data_rendering, name="my-data-render")
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,3 +14,16 @@ def add(request, a, b):
 def fact(request, num):
     from functools import reduce
     return HttpResponse(f"fact of {num}: {reduce(lambda x, y: x * y, range(1, num+1))}")
+
+
+def data_rendering(request):
+    data = [
+        {"gender": "male", "first_name": "suresh", "last_name": "kumar", "age": 25,
+         "tech": ["python", "django", "selenium"]},
+        {"gender": "female", "first_name": "sandya", "last_name": "kumari", "age": 25, "tech": ["Java", "springs"]},
+        {"gender": "male", "first_name": "suresh", "last_name": "kumar", "age": 25,
+         "tech": ["python", "django", "selenium"]},
+        {"gender": "female", "first_name": "keerthi", "last_name": "yadav", "age": 25, "tech": ["c", "networks", "linux"]},
+        {"gender": "female", "first_name": "taja", "last_name": "aswani", "age": 25, "tech": [".net", "c#", "powerBI"]}
+    ]
+    return render(request, template_name="render.html", context={"mydata": data})
